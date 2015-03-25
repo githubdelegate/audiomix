a12159bb1a1383320acbc588eaf9c7913da1a9f6
zhangyun <zhangyun@yunzhisheng.cn>
2015-03-25 15:55:48 +0800

    init commit
    init the file

---
+++ audiomix/audiomix/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  audiomix
+//
+//  Created by iOSDevelope on 15-3-25.
+//  Copyright (c) 2015年 usc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
